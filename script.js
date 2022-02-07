a2ca43ae92198a1d8e4e92079502c6c39a09aed6
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-07 18:31:50 +0500

    Rebuilding app part -1

--- script.js
+++ script.js
@@ -1,92 +1,44 @@
 'use strict';
-// Selecting elemnts
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const player0El = document.querySelector('.player--1');
+const player1El = document.querySelector('.player--0');
 
-let scores, currentScore, activePlayer, playing;
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
 
-// Starting conditions
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
-const init = function () {
-  score1El.textContent = 0;
-  score0El.textContent = 0;
-  diceEl.classList.add('hidden');
-
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-};
-init();
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  currentScore = 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-  diceEl.classList.add('hidden');
-};
-
-// Rollidng dice functionality
 btnRoll.addEventListener('click', function () {
-  if (playing) {
-    // 1. Generate a random dice rollidng
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    // 2. Display dice roll
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-    // 3. Check for rolled 1
-    if (dice !== 1) {
-      // Add dice to current score
-      currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
-    } else {
-      // Switch to the next player
-      switchPlayer();
-      diceEl.classList.remove('hidden');
-    }
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
 
-// Btn hold clecked
 btnHold.addEventListener('click', function () {
-  if (playing) {
-    diceEl.classList.add('hidden');
-    // 1. Add current score to the active player's score
-    scores[activePlayer] += currentScore;
-    // scores[1] == scores[1] + currentScore
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
-    // 2. check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
-      playing = false;
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
-    } else {
-      // Switch to the next player
-      switchPlayer();
-    }
-  }
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
 });
-
-// New game
-btnNew.addEventListener('click', init);
